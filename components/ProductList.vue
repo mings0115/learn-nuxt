5b5123db5dd78ccdd72476e1fe6db31c4d2f7bd4
mings0115 <mings9201@gmail.com>
2022-05-08 19:14:39 +0900

    26강 - 상품 목폭 표시 기능 구현

---
+++ components/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ products }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response.data
+    return { products }
+  },
+}
+</script>
+
+<style></style>
