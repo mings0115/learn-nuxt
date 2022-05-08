dafc8da5041e62ed06883e52d79e1ba1f7eafee1
mings0115 <mings9201@gmail.com>
2022-05-08 18:31:59 +0900

    23강 - asyncData 사용

--- pages/main.vue
+++ pages/main.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
     <p>메인 페이지입니다.</p>
+    <div>
+      {{ products }}
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response.data
+    return { products }
+  },
+}
 </script>
 
 <style></style>
