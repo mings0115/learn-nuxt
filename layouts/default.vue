d1bcaf7fe34a54c562bd04a41d5add681ec129df
mings0115 <mings9201@gmail.com>
2022-05-31 20:11:19 +0900

    37 강 - 헤더와 검색 UI 스타일 정리

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <header>
-      <h1><NuxtLink to="/">Nuxt Shopping</NuxtLink></h1>
+      <header class="title">
+        <nuxt-link class="logo" to="/">Nuxt Shopping</nuxt-link>
+      </header>
     </header>
     <Nuxt />
   </div>
@@ -11,4 +13,18 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+/* 헤더 관련 스타일 */
+header {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+}
+.logo {
+  font-weight: 900;
+}
+.logo:visited {
+  color: inherit;
+}
+</style>
