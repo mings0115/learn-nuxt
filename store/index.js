841e15e8782be679bb7ff1922fd5839edf3a59e1
mings0115 <mings9201@gmail.com>
2022-06-01 13:07:38 +0900

    50강 - nuxtServerlnit 적용 및 특성 안내

--- store/index.js
+++ store/index.js
@@ -15,7 +15,7 @@ export const mutations = {
       }
         state.cartItems.push(newCartItem)
     },
-    setCartItems(state, cartItems) {
+    setCartItems(state, cartItems) { 
         state.cartItems = cartItems
     }
 }
@@ -23,6 +23,12 @@ export const mutations = {
 export const actions = {
     async [FETCH_CART_ITEMS] ({ commit }) {
         const { data } =  await fetchCartItems();
-        commit('setCartItems', data)
-    }
+        commit('setCartItems', data.map(item => ({
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      })))
+    },
+    async nuxtServerInit(storeContext, nuxtContext) {
+        await storeContext.dispatch(FETCH_CART_ITEMS)
+    },
 }
