1c9727839ba613065821095c22f0c9abd55ed14f
mings0115 <mings9201@gmail.com>
2022-06-01 15:04:42 +0900

    61강 - 서버 에러 분석 및 에러 수정

--- api/index.js
+++ api/index.js
@@ -4,6 +4,10 @@ const instance = axios.create({
     baseURL: process.env.baseURL,
 })
 
+function fetchProducts() {
+    return instance.get('/products')
+}
+
 function fetchProductById(id) {
     return instance.get(`/products/${id}`)
 }
@@ -22,4 +26,10 @@ function createCartItem(cartItem) {
     return instance.post('/carts', cartItem)
 }
 
-export { fetchProductById, fetchProductsByKeyword, createCartItem, fetchCartItems }
+export { 
+    fetchProducts, 
+    fetchProductById, 
+    fetchProductsByKeyword, 
+    createCartItem, 
+    fetchCartItems 
+}
